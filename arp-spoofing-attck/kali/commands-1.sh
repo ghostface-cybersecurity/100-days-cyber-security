a6afda91d43a873c989a213595553abb0cbb778a
hellcard <wseven347.devopser@gmail.com>
2025-01-04 21:58:40 -0700

    Create commands-1.sh

---
+++ arp-spoofing-attck/kali/commands-1.sh
@@ -0,0 +1,6 @@
+# Tab-1
+sudo -i # root
+apt-get update 
+apt-get install dsniff # install dsniff for use arpspoof tool
+
+netdiscover # scans the network for IP & MAC addresses, sending arp-requests to everyone IP-adresses
