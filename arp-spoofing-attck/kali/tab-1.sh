0165faf477f8bde89a327c050c84e5b39f1ce52b
hellcard <wseven347.devopser@gmail.com>
2025-01-04 22:05:11 -0700

    Update tab-1.sh

--- arp-spoofing-attck/kali/tab-1.sh
+++ arp-spoofing-attck/kali/tab-1.sh
@@ -4,3 +4,6 @@ apt-get update
 apt-get install dsniff # install dsniff for use arpspoof tool
 
 netdiscover # scans the network for IP & MAC addresses, sending arp-requests to everyone IP-adresses
+# output:
+# 192.168.1.1 - since this IP address is lower, it means it is a router
+# 192.168.1.101 - this means this is the "victim's computer" (metasploitable2)
