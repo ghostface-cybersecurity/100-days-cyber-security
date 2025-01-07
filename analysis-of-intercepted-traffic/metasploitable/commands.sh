9d930f3e4a23bf56fb779db1bec3c8c3547d00a8
hellcard <wseven347.devopser@gmail.com>
2025-01-06 20:58:29 -0700

    Update commands.sh

--- analysis-of-intercepted-traffic/metasploitable/commands.sh
+++ analysis-of-intercepted-traffic/metasploitable/commands.sh
@@ -1,4 +1,8 @@
 msfadmin
 msfadmin
 
-ifconfig eth0 # 
+ifconfig eth0 # read the IP address of the metasploitable2 virtual machine
+# OUTPUT
+# /---/ <- another info
+# inet addr:192.168.1.101 <- IP address  
+# /---/ <- another info
