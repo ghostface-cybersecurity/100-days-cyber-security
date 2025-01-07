c1a66aec5cb844f88b0df76856d41c873c1a6ba6
hellcard <wseven347.devopser@gmail.com>
2025-01-06 23:07:14 -0700

    Update commands.sh

--- analysis-of-intercepted-traffic/pfsense/commands.sh
+++ analysis-of-intercepted-traffic/pfsense/commands.sh
@@ -1,3 +1,14 @@
-8 # open shell
+8 # open shell from main menu
 tcpdump # in this case, all packets passing through the system will be intercepted
 # (ctrl+C) for exit
+
+
+# you can also view packets passing through a specific port, for example 443
+tcpdump tcp port 443 -n
+# or
+tcpdump tcp port 443
+
+# you can also save a record of these packages to a file
+tcpdump -i <interface> <number of packages> -w <filename.pcap>
+
+exit # return to main menu
